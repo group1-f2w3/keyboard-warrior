f8c387af7d9381863e07807a86d817926d473ea7
rey-h8 <rey-h8@gmail.com>
2020-11-13 01:49:58 +0700

    fix double imports in app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,23 +2,18 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-<<<<<<< HEAD
       <router-link to="/typing">Typing</router-link>
-      <router-link to="/about">About</router-link> |
       <router-link to="/fight">Fight</router-link>
-=======
-      <router-link to="/about">About</router-link> |
       <router-link to="/welcome">Welcome</router-link> |
       <router-link to="/arena">Arena</router-link> |
-      <router-link to="/result">Result</router-link> 
->>>>>>> b6c2ea1ef444fba4829c7e45342afe6066220de9
+      <router-link to="/result">Result</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
-<<<<<<< HEAD
+  @import './assets/css/style.css';
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -26,16 +21,6 @@
     text-align: center;
     color: #2c3e50;
   }
-=======
-@import './assets/css/style.css';
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
->>>>>>> b6c2ea1ef444fba4829c7e45342afe6066220de9
 
   #nav {
     padding: 30px;
@@ -46,14 +31,10 @@
     color: #2c3e50;
   }
 
-<<<<<<< HEAD
   #nav a.router-link-exact-active {
     color: #42b983;
   }
-=======
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-@import './assets/css/style.css';
->>>>>>> b6c2ea1ef444fba4829c7e45342afe6066220de9
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
 </style>
