8ef0ae886030688fb6afca7ba240f37634d9068d
dickyandeyan <dickyundead@gmail.com>
2020-11-13 11:22:20 +0700

    add character and arena background

--- client/src/views/Arena.vue
+++ client/src/views/Arena.vue
@@ -4,20 +4,21 @@
         <div class="container">
           <div class="row">
             <div class="col">
-              <h3>health bar player 1</h3>
+              <h3 class="text-white">health bar player 1</h3>
               <div id="myProgress1">
                 <div id="myBar1"></div>
               </div>
             </div>
             <div class="col">
-              <h3>health bar player 2</h3>
+              <h3 class="text-white">health bar player 2</h3>
               <div id="myProgress2">
                 <div id="myBar2"></div>
               </div>
             </div>
           </div>
-          <div class="row">
-            <!-- Buat characterr -->
+          <div class="row d-flex mt-5">
+              <img :src="knight" width="50%" alt="" srcset="">
+              <img :src="knightEnemy" width="50%" alt="" srcset="">
           </div>
         </div>
       </div>
@@ -31,8 +32,16 @@
 </template>
 
 <script>
+import knight from '@/assets/knight-a-idle.gif'
+import knightEnemy from '@/assets/knight-b-idle.gif'
 export default {
   name: 'Arena',
+  data () {
+    return {
+      knight,
+      knightEnemy
+    }
+  }
 }
 </script>
 
